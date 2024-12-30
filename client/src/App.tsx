9cc3418205a16a17ec54aff79475318beef0a4a0
Jeff2hu <lovezhu661991@gmail.com>
2024-12-30 17:29:50 +0800

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+// import { lazy, Suspense, useState } from "react";
+// import ClickMe from "./ClickMe";
+// const ClickMes = lazy(() => import("./ClickMe"));
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  // const handleClick = () => {
+  //   setCount(count + 1);
+  // };
+
+  return (
+    <div>
+      {/* <ClickMe count={count} handleClick={handleClick} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <></>
+      </Suspense> */}
+      Hello world
+    </div>
+  );
+}
+
+export default App;
